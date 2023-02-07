96130138873ba3fe21ad64eeb0ebfbe5834343fd
guillaume-gricourt <guipagui@gmail.com>
2023-02-07 21:16:31 +0100

    tests(hmnqc): complete test_functional(), update dataset

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 import os
+from collections import namedtuple
 from typing import Dict, Tuple
 
 import pytest
 
 cur_dir = os.path.abspath(os.path.dirname(__file__))
 data_dir = os.path.join(cur_dir, "dataset")
+
+Sample = namedtuple(
+    "Sample", ["fq_fwd_raw", "fq_rev_raw", "fq_fwd_trim", "fq_rev_trim", "bam", "vcf"]
+)
 
 
 @pytest.fixture(scope="session")
@@ -13,31 +18,33 @@ def data_directory():
 
 
 @pytest.fixture(scope="session")
-def one_fq_raw(data_directory) -> Tuple[str, str]:
-    return (
-        os.path.join(data_directory, "one.R1.fastq"),
-        os.path.join(data_directory, "one.R2.fastq"),
+def sample_one(data_directory) -> Tuple[str, str]:
+    return Sample(
+        fq_fwd_raw=os.path.join(data_directory, "one.R1.fastq"),
+        fq_rev_raw=os.path.join(data_directory, "one.R2.fastq"),
+        fq_fwd_trim=os.path.join(data_directory, "one.R1.trim.fastq"),
+        fq_rev_trim=os.path.join(data_directory, "one.R2.trim.fastq"),
+        bam=os.path.join(data_directory, "one.bam"),
+        vcf=os.path.join(data_directory, "one.vcf"),
     )
 
 
 @pytest.fixture(scope="session")
-def one_fq_trim(data_directory) -> Tuple[str, str]:
-    return (
-        os.path.join(data_directory, "one.R1.trim.fastq"),
-        os.path.join(data_directory, "one.R2.trim.fastq"),
-    )
+def one_bed(data_directory) -> str:
+    return os.path.join(data_directory, "one.bed")
 
 
 @pytest.fixture(scope="session")
-def one_bam(data_directory) -> str:
-    return os.path.join(data_directory, "one.bam")
-
-
-@pytest.fixture(scope="session")
-def one_vcf(data_directory) -> str:
-    return os.path.join(data_directory, "one.vcf")
+def reference_vcf(data_directory) -> str:
+    return os.path.join(data_directory, "reference.vcf")
 
 
 @pytest.fixture(scope="session")
 def one_results(data_directory) -> Dict[str, str]:
-    return dict(quality=os.path.join(data_directory, "one.quality.json"))
+    return dict(
+        quality=os.path.join(data_directory, "one.quality.json"),
+        depthmin=os.path.join(data_directory, "one.depthmin.xlsx"),
+        depthtarget=os.path.join(data_directory, "one.depthtarget.xlsx"),
+        infersexe=os.path.join(data_directory, "one.infersexe.xlsx"),
+        extractvcf=os.path.join(data_directory, "one.extractvcf.xlsx"),
+    )
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,15 +1,17 @@
+import filecmp
 import json
 import sys
 import tempfile
 from typing import List
 
+import pandas as pd
 from hmnqc._version import __app_name__
 from hmnqc.utils import run
 
 
 class TestFunctional:
     @classmethod
-    def compare_json(cls, one, two, del_keys: List[str] = None):
+    def compare_json(cls, one, two, del_keys: List[str] = None) -> bool:
         with open(one) as fod:
             one_data = json.load(fod)
         with open(two) as fod:
@@ -21,17 +23,23 @@ class TestFunctional:
 
         return one_data == two_data
 
-    def test_quality(self, one_fq_raw, one_fq_trim, one_bam, one_vcf, one_results):
+    @classmethod
+    def compare_xlsx(cls, one, two) -> bool:
+        df1 = pd.read_excel(one)
+        df2 = pd.read_excel(two)
+        return df1.equals(df2)
+
+    def test_quality(self, sample_one, one_results):
         with tempfile.NamedTemporaryFile() as fd:
             args = [__app_name__, "quality"]
             args += ["--input-sample-name", "one"]
             args += ["--output-hmnqc-json", fd.name]
-            args += ["-iffr", one_fq_raw[0]]
-            args += ["-ifrr", one_fq_raw[1]]
-            args += ["-ifft", one_fq_trim[0]]
-            args += ["-ifrt", one_fq_trim[1]]
-            args += ["--input-sample-bam", one_bam]
-            args += ["--input-sample-vcf", one_vcf]
+            args += ["-iffr", sample_one.fq_fwd_raw]
+            args += ["-ifrr", sample_one.fq_rev_raw]
+            args += ["-ifft", sample_one.fq_fwd_trim]
+            args += ["-ifrt", sample_one.fq_rev_trim]
+            args += ["--input-sample-bam", sample_one.bam]
+            args += ["--input-sample-vcf", sample_one.vcf]
 
             ret = run(args)
             if ret.returncode > 0:
@@ -42,3 +50,67 @@ class TestFunctional:
             assert TestFunctional.compare_json(
                 fd.name, one_results["quality"], del_keys=["files"]
             )
+
+    def test_depthmin(self, sample_one, one_bed, one_results):
+        with tempfile.NamedTemporaryFile(suffix=".xlsx") as fd:
+            args = [__app_name__, "depthmin"]
+            args += ["--input-sample-bam", sample_one.bam]
+            args += ["--output-hmnqc-xlsx", fd.name]
+            args += ["--input-sample-bed", one_bed]
+            # --parameter-cut-off <int>
+
+            ret = run(args)
+            if ret.returncode > 0:
+                print(ret.stderr)
+                print(ret.stdout)
+                sys.exit(1)
+
+            assert TestFunctional.compare_xlsx(fd.name, one_results["depthmin"])
+
+    def test_depthtarget(self, sample_one, one_bed, one_results):
+        with tempfile.NamedTemporaryFile(suffix=".xlsx") as fd:
+            args = [__app_name__, "depthtarget"]
+            args += ["--input-sample-bam", sample_one.bam]
+            args += ["--output-hmnqc-xlsx", fd.name]
+            args += ["--input-sample-bed", one_bed]
+            # --parameter-mode target \
+
+            ret = run(args)
+            if ret.returncode > 0:
+                print(ret.stderr)
+                print(ret.stdout)
+                sys.exit(1)
+
+            assert TestFunctional.compare_xlsx(fd.name, one_results["depthtarget"])
+
+    def test_infersexe(self, sample_one, one_bed, one_results):
+        with tempfile.NamedTemporaryFile(suffix=".xlsx") as fd:
+            args = [__app_name__, "depthtarget"]
+            args += ["--input-sample-bam", sample_one.bam]
+            args += ["--output-hmnqc-xlsx", fd.name]
+            args += ["--input-sample-bed", one_bed]
+            # --input-sample-bed <BED file> \
+
+            ret = run(args)
+            if ret.returncode > 0:
+                print(ret.stderr)
+                print(ret.stdout)
+                sys.exit(1)
+
+            assert TestFunctional.compare_xlsx(fd.name, one_results["infersexe"])
+
+    def test_extractvcf(self, sample_one, reference_vcf, one_results):
+        out = "/save/ggricourt/opt/guillaume-gricourt/HmnQc/tests/one.extractvcf.xlsx"
+        with tempfile.NamedTemporaryFile(suffix=".xlsx") as fd:
+            args = [__app_name__, "extractvcf"]
+            args += ["--input-sample-vcf", sample_one.vcf]
+            args += ["--input-reference-vcf", reference_vcf]
+            args += ["--output-hmnqc-xlsx", fd.name]
+
+            ret = run(args)
+            if ret.returncode > 0:
+                print(ret.stderr)
+                print(ret.stdout)
+                sys.exit(1)
+
+            assert TestFunctional.compare_xlsx(fd.name, one_results["extractvcf"])
